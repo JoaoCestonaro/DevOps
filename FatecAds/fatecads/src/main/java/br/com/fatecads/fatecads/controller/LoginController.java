db2bb7037019d675d51fc23302113e78c6c73967
JoaoCestonaro <joaoantoniogms938@gmail.com>
2026-05-18 17:41:57 -0300

    Adicionando Login funcional e Cadastro de Usuário

--- FatecAds/fatecads/src/main/java/br/com/fatecads/fatecads/controller/LoginController.java
+++ FatecAds/fatecads/src/main/java/br/com/fatecads/fatecads/controller/LoginController.java
@@ -1,15 +1,14 @@
 package br.com.fatecads.fatecads.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
+
     @GetMapping("/login")
     public String login() {
         return "login";
